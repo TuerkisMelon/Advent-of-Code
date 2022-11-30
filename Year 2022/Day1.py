93092cdcb7095cdb7d980a60ac90a612670ada29
TuerkisMelon <95377151+TuerkisMelon@users.noreply.github.com>
2022-11-30 21:32:17 +0100

    Update Day1.py

--- Year 2022/Day1.py
+++ Year 2022/Day1.py
@@ -1,1 +1,8 @@
+from get_file_from_server import get
 
+if __name__ == "__main__":
+    g = get()
+    session_cookie = str(input("insert session cookie>"))
+    inp = get("https://adventofcode.com/2022/day/1/input", {"session": session_cookie})
+
+    
