d82680f7a32018121fbdbcbfb3b9ab456187b348
TuerkisMelon <95377151+TuerkisMelon@users.noreply.github.com>
2023-09-09 17:36:54 +0200

    Update Day6.py

--- Year 2022/Day6/Day6.py
+++ Year 2022/Day6/Day6.py
@@ -1,1 +1,34 @@
+with open("input.txt", "r") as f:
+    inp = f.read()
 
+#1
+marker = ""
+marker_pos = -1
+for i in range(len(inp)-4):
+    if len(marker) == 4:
+        break
+    else:
+        marker_pos += 1
+        marker = inp[marker_pos:marker_pos+4]
+        print(marker)
+        for char in marker:
+            if len(marker.replace(char, "")) != 3:
+                marker = ""
+
+print(f"Aufg 1: {marker_pos + 4}")
+
+#2
+marker = ""
+marker_pos = -1
+for i in range(len(inp)-14):
+    if len(marker) == 14:
+        break
+    else:
+        marker_pos += 1
+        marker = inp[marker_pos:marker_pos+14]
+        print(marker)
+        for char in marker:
+            if len(marker.replace(char, "")) != 13:
+                marker = ""
+
+print(f"Aufg 2: {marker_pos + 14}")
